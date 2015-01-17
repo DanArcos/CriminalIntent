9cf1824e2ee8bc4cb7dcd007f49b0d5d78ed1b61
DArcos <danarc0s7@gmail.com>
2015-01-17 11:13:12 -0500

    Added onCreateView method and inflated it

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeFragment.java
@@ -1,15 +1,30 @@
 package com.cornbread.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 //Notice this fragment class takes from the support library!
 public class CrimeFragment extends Fragment{
     private Crime mCrime; //Crime object
 
+    //This is a public function whereas Activity.onCreate is a protected
+    //This must be public so that the activity (a different) can call it.
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    //This function inflates the view
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup parent,
+                             @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+        return v;
     }
 }
