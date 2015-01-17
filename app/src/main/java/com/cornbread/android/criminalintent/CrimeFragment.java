91439078b201b586f62877a3edcdc92ea0096b3a
DArcos <danarc0s7@gmail.com>
2015-01-17 11:28:01 -0500

    Tied titleField to the crime_fragment. Created on text changed listener.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeFragment.java
@@ -3,13 +3,17 @@ package com.cornbread.android.criminalintent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 //Notice this fragment class takes from the support library!
 public class CrimeFragment extends Fragment{
     private Crime mCrime; //Crime object
+    private EditText mTitleField;
 
     //This is a public function whereas Activity.onCreate is a protected
     //This must be public so that the activity (a different) can call it.
@@ -25,6 +29,25 @@ public class CrimeFragment extends Fragment{
                              @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+                mCrime.setTitle(c.toString()); //When Text is changed title is changed
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //This space intentionally left blank
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //This one too
+            }
+        });
+
         return v;
     }
 }
