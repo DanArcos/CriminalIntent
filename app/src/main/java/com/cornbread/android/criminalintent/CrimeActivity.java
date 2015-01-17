90cfddf271970c40c1809f843ca67fe84d2f6de1
DArcos <danarc0s7@gmail.com>
2015-01-17 10:10:21 -0500

    Added comment

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+//Fragment Activity is support class
 public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
