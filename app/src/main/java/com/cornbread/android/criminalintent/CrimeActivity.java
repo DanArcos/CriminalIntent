4a54e1fb9a601ca8603556304863a499861fa08e
DArcos <danarc0s7@gmail.com>
2015-01-17 13:53:30 -0500

    Added Fragment Manager to properly load up widgets

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.cornbread.android.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,5 +14,20 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        //Fragments are identified by the resource ID of its container layout
+        //In this case, R.id.fragmentContainer should contain the fragment, and is not the fragment itself
+        // fragmentManager.findFragmentById(R.id.fragmentContainer) should return the CrimeFragment() if it exists
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+
+        //If there is no fragment, create one
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment) //(container view ID, fragment)
+                    .commit();
+        }
     }
 }
