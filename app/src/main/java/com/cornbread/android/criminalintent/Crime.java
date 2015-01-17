0ba9a126e30002a6fb8b165e5e1165b75a7cf7e7
DArcos <danarc0s7@gmail.com>
2015-01-17 14:18:36 -0500

    Added more variables to crime model. Added more getters and setters.

--- app/src/main/java/com/cornbread/android/criminalintent/Crime.java
+++ app/src/main/java/com/cornbread/android/criminalintent/Crime.java
@@ -1,14 +1,19 @@
 package com.cornbread.android.criminalintent;
 
+import org.joda.time.DateTime;
+
 import java.util.UUID;
 
 public class Crime {
     private UUID mId; //UUID are unique IDs to identify the crime object
     private String mTitle; //String to name crime object
+    private DateTime mDate;
+    private boolean mSolved;
 
     public Crime(){
         //Generate Unique Identifier
         mId = UUID.randomUUID();
+        mDate = new DateTime();
     }
 
     //We only create a getter for the UUID
@@ -24,5 +29,21 @@ public class Crime {
     //Setter for title
     public void setTitle(String title){
         mTitle = title;
+    }
+
+    public DateTime getDate(){
+        return mDate;
+    }
+
+    public void setDate (DateTime date){
+        mDate = date;
+    }
+
+    public boolean isSolved(){
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved){
+        mSolved = solved;
     }
 }
