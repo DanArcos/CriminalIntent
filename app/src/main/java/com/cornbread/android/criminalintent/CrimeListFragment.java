664d26b9033273c9e79be9965aaaa16c00a09d5e
DArcos <danarc0s7@gmail.com>
2015-01-19 15:44:23 -0500

    Updated custom list item view to change titleTextView to display actual title of the Crime.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeListFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -48,6 +49,12 @@ public class CrimeListFragment extends ListFragment{
             if(convertView == null){
                 convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime,null);
             }
+
+            //Configure the view for this Crime
+            Crime c = getItem(position);
+
+            TextView titleTextView = (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
 
             return convertView;
         }
