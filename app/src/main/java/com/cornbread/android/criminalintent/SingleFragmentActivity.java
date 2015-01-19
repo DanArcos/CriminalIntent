bd09f255d0e06845f159d7598842fd8c11f93004
DArcos <danarc0s7@gmail.com>
2015-01-19 13:36:53 -0500

    Created abstract class SingleFragmentActivity to make fragment transactions easy. The only work required is to create a new fragment with which you want to attach.

--- app/src/main/java/com/cornbread/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/cornbread/android/criminalintent/SingleFragmentActivity.java
@@ -1,27 +1,26 @@
 package com.cornbread.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
-//Fragment Activity is support class versus regular Fragment
-public class CrimeActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends FragmentActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment); //Inflate view from generic layout
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        //Fragments are identified by the resource ID of its container layout
-        //In this case, R.id.fragmentContainer should contain the fragment, and is not the fragment itself
-        // fragmentManager.findFragmentById(R.id.fragmentContainer) should return the CrimeFragment() if it exists
+        /*  Fragments are identified by the resource ID of its container layout. In this case, R.id.fragmentContainer should
+            contain the fragment, and is not the fragment itself fragmentManager.findFragmentById(R.id.fragmentContainer)
+            should return the CrimeFragment() if it exists */
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
 
         //If there is no fragment, create one
         if(fragment == null){
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fragmentManager.beginTransaction()
                     .add(R.id.fragmentContainer, fragment) //(container view ID, fragment)
                     .commit();
