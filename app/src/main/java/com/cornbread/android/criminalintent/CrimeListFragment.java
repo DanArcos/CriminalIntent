86d23bc08ad68bb9af5ba7f2df9d34427f2a3915
DArcos <danarc0s7@gmail.com>
2015-01-19 15:21:44 -0500

    Overrode onListItemClick method to understand its functionality.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeListFragment.java
@@ -2,11 +2,16 @@ package com.cornbread.android.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class CrimeListFragment extends ListFragment{
+    private static final String TAG = "CrimeListFragment";
+
     private ArrayList<Crime> mCrimes;
 
     @Override
@@ -22,5 +27,11 @@ public class CrimeListFragment extends ListFragment{
         //Parameter 3: Choose data this will be used to populate list (mCrime)
         ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),android.R.layout.simple_list_item_1,mCrimes);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter().getItem(position)); //Pull crime by its position in the list.
+        Log.d(TAG, c.getTitle()+" was clicked.");
     }
 }
