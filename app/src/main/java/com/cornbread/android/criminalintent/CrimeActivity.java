d49cc513a98539a3cb3aa2eddb9f0aea6e866506
DArcos <danarc0s7@gmail.com>
2015-01-19 13:21:34 -0500

    Renamed activity_crime -> activity_fragment since we can use the same layout (FrameLayout) for all activities that will host fragments.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeActivity.java
@@ -3,17 +3,14 @@ package com.cornbread.android.criminalintent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 //Fragment Activity is support class versus regular Fragment
 public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
