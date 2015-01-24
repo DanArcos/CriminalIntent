8120d21ad97c99343e710891cabb00e09e569b4a
DArcos <danarc0s7@gmail.com>
2015-01-24 17:52:42 -0500

    Created ID for dialog_date layout. Created view in onCreateDialog and inflated the xml dialog date to it.

--- app/src/main/java/com/cornbread/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/DatePickerFragment.java
@@ -5,15 +5,19 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 //A second fragment that will be managed by CrimePagerActivity
 public class DatePickerFragment extends DialogFragment{
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title) //Title of Dialog
-                .setPositiveButton(android.R.string.ok,null) //Positive Button: what the user should press to accept what the dialog presents
+                .setPositiveButton(android.R.string.ok, null) //Positive Button: what the user should press to accept what the dialog presents
                 .create(); //"Commits" to creating the dialog
     }
 }
