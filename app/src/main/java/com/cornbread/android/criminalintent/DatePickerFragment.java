393a866e0ab30ba7ab351339cac58fe89b395606
DArcos <danarc0s7@gmail.com>
2015-01-29 23:28:19 -0500

    Initialized DatePicker and wrote code to set new mDate to date that we choose.

--- app/src/main/java/com/cornbread/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/DatePickerFragment.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.widget.DatePicker;
 
 import org.joda.time.DateTime;
+
+import java.util.GregorianCalendar;
 
 //A second fragment that will be managed by CrimePagerActivity
 public class DatePickerFragment extends DialogFragment{
@@ -27,7 +30,25 @@ public class DatePickerFragment extends DialogFragment{
     //a dialog is not the same as a fragment
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate = (DateTime)getArguments().getSerializable(EXTRA_DATE); //Retrieve DateTime info
+
+        //We must pull out the following so that we ca properly intitialize the datePicker
+        int year = mDate.getYear();
+        int month = mDate. getMonthOfYear() - 1; //Jan = 0,
+        int day = mDate.getDayOfMonth();
+
         View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+
+        DatePicker datePicker = (DatePicker)v.findViewById(R.id.dialog_date_picker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                mDate = new DateTime(new GregorianCalendar(year, monthOfYear, dayOfMonth)); //Use selected date to create new mDate
+
+                //Update argument to preserve selected value on rotation
+                getArguments().putSerializable(EXTRA_DATE, mDate);
+            }
+        });
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
