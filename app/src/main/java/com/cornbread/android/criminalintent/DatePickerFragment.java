0c898a3fdd29b6646adce3eee79242bfc6b6193d
DArcos <danarc0s7@gmail.com>
2015-01-29 23:05:46 -0500

    Added string as key for pulling out Date info in extras and serializiables. Added DateTime mDate to store date. Added new Instance method so that a DatePicker fragment can be created with stored DateTime info.

--- app/src/main/java/com/cornbread/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/DatePickerFragment.java
@@ -7,9 +7,24 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 
+import org.joda.time.DateTime;
+
 //A second fragment that will be managed by CrimePagerActivity
 public class DatePickerFragment extends DialogFragment{
-    @NonNull
+    public static final String EXTRA_DATE = "com.cornbread.android.criminalintent.date"; //String to help store intent extras
+    private DateTime mDate; // Date which we will use to store the value of the date info
+
+    public static DatePickerFragment newInstance(DateTime date){
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_DATE, date); //Put away date info as key value pairing
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args); //"store arguments"
+
+        return fragment;
+    }
+
+    //a dialog is not the same as a fragment
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
