8b847edef492e9e2f6ffd655946210aa44f8c39e
DArcos <danarc0s7@gmail.com>
2015-02-16 22:08:08 -0500

    Added code to toggle subtitle on off.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeListFragment.java
@@ -110,7 +110,14 @@ public class CrimeListFragment extends ListFragment{
                 return true;
 
             case R.id.menu_item_show_subtitle:
-                getActivity().getActionBar().setSubtitle(R.string.subtitle);
+                if(getActivity().getActionBar().getSubtitle() == null){
+                    getActivity().getActionBar().setSubtitle(R.string.subtitle);
+                    item.setTitle(R.string.hide_subtitle);
+                } else {
+                    getActivity().getActionBar().setSubtitle(null);
+                    item.setTitle(R.string.show_subtitle);
+                }
+
                 return true;
 
             default:
