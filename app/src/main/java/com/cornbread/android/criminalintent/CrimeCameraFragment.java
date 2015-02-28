9bbc426f47102f04cb76b09192adebf870f79a0c
DArcos <danarc0s7@gmail.com>
2015-02-28 18:24:03 -0500

    Inflate layout and get references to widgets. The Button at this moment acts as a place holder nothing more!

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeCameraFragment.java
@@ -1,6 +1,35 @@
 package com.cornbread.android.criminalintent;
 
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class CrimeCameraFragment extends Fragment {
+    private static final String TAG = "CrimeCameraFragment";
+
+    private Camera camera; //Camera object
+    private SurfaceView mSurfaceView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime_camera, container);
+
+        Button takePictureButton = (Button) v.findViewById(R.id.crime_camera_takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish(); //Simply finishes hosting activity this will be updated.
+            }
+        });
+
+        mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+
+        return v;
+    }
 }
