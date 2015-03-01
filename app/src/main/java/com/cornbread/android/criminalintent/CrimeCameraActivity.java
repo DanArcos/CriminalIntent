d9de07ac6fc6919c1af9ff57342a62592ef2876c
DArcos <danarc0s7@gmail.com>
2015-02-28 22:14:53 -0500

    Added code to remove action bar when camera is launched.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeCameraActivity.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeCameraActivity.java
@@ -1,10 +1,24 @@
 package com.cornbread.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CrimeCameraActivity extends SingleFragmentActivity{
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Hide window title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Hide the status bar and other os-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
     }
 }
