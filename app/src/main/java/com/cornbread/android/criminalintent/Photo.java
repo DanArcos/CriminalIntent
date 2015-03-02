6377ceb68779643ebacba148245ef880684a2d88
DArcos <danarc0s7@gmail.com>
2015-03-01 21:42:14 -0500

    Added 2 constructors, toJSON mehtods, and getFileName method to the photo class.

--- app/src/main/java/com/cornbread/android/criminalintent/Photo.java
+++ app/src/main/java/com/cornbread/android/criminalintent/Photo.java
@@ -1,4 +1,32 @@
 package com.cornbread.android.criminalintent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Photo {
+    private static final String JSON_FILENAME = "filename";
+
+    private String mFileName;
+
+    //Create a photo representing an existing file on disk
+
+    //Constructor with filename string as parameter
+    public Photo(String filename){
+        mFileName = filename;
+    }
+
+    //Constructor with json object as parameter. The crime class will use this.
+    public Photo(JSONObject json) throws JSONException{
+        mFileName = json.getString(JSON_FILENAME);
+    }
+
+    public JSONObject toJSON() throws JSONException{
+        JSONObject json = new JSONObject();
+        json.put(JSON_FILENAME, mFileName);
+        return json;
+    }
+
+    public String getFileName(){
+        return mFileName;
+    }
 }
