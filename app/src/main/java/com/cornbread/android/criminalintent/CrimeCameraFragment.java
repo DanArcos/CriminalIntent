b5371e1ee00cdb8717bac5669e12e023000e4afc
DArcos <danarc0s7@gmail.com>
2015-03-01 21:05:24 -0500

    Added code to create intent to put file name as activity result.

--- app/src/main/java/com/cornbread/android/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/cornbread/android/criminalintent/CrimeCameraFragment.java
@@ -1,7 +1,9 @@
 package com.cornbread.android.criminalintent;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,6 +24,7 @@ import java.util.UUID;
 
 public class CrimeCameraFragment extends Fragment {
     private static final String TAG = "CrimeCameraFragment";
+    public static final String EXTRA_PHOTO_FILENAME = "com.cornbread.android.criminalintent.photo_filename";
 
     private android.hardware.Camera mCamera; //Camera object
     private SurfaceView mSurfaceView;
@@ -64,6 +67,11 @@ public class CrimeCameraFragment extends Fragment {
 
             if(success){
                 Log.i(TAG, "JPEG saved at " + filename);
+                Intent i = new Intent();
+                i.putExtra(EXTRA_PHOTO_FILENAME, filename);
+                getActivity().setResult(Activity.RESULT_OK, i);
+            } else {
+                getActivity().setResult(Activity.RESULT_CANCELED);
             }
             getActivity().finish();
         }
